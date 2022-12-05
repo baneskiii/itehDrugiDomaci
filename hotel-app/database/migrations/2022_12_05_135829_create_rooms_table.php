c0b00dab96c39c8c02c043278de7a62fd94e4a88
Branislav Trajkov <branislav.trajkov00@gmail.com>
2022-12-05 15:17:54 +0100

    Kreiranje migracija

--- hotel-app/database/migrations/2022_12_05_135829_create_rooms_table.php
+++ hotel-app/database/migrations/2022_12_05_135829_create_rooms_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->integer('floor');
+            $table->integer('area_in_m^2');
+            $table->integer('number_of_beds');
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('rooms');
     }
 };
--- hotel-app/database/migrations/2022_12_05_140541_create_reservations_table.php
+++ hotel-app/database/migrations/2022_12_05_140541_create_reservations_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->date('date_from');
+            $table->date('date_to');
+            $table->foreignId('room_id');
+            $table->foreignId('guest_id');
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('reservations');
     }
 };
